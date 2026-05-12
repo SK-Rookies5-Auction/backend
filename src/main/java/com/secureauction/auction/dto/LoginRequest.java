4759e19f4b6bea039c6ec866ea0fbc0f36482e46
Eojinn <aj41212@gmail.com>
2026-05-12 13:41:49 +0900

    feat: 경매 상품 찜하기 기능 및 마이페이지 연동

--- src/main/java/com/secureauction/auction/dto/LoginRequest.java
+++ src/main/java/com/secureauction/auction/dto/LoginRequest.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "아이디를 입력해주세요.")
-    @JsonProperty("login_id")
     private String loginId;
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
